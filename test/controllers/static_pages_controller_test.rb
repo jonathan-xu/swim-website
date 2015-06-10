56446aab1f2b3131b0af6123f0824bdfdebe7dc8
Jonathan <jonathan.xu3@gmail.com>
2015-06-10 00:19:59 -0700

    finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Cal Men's Swim and Dive"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Cal Men's Swim and Dive"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | Cal Men's Swim and Dive"
   end
 
 end
