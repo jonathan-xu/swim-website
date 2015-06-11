a2114f93d2c1356e4d14c67e0d847c95db94bc25
Jonathan <jonathan.xu3@gmail.com>
2015-06-11 13:48:49 -0700

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  # lalala doing comment code!
+  def full_title(page_title = '')
+    base_title = "Cal Men's Swim and Dive"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
